e63dcd63209cf603a06e11dfac1eeaf0b43734c2
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2023-12-04 19:15:45 +0530

    use effect and fetch data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
+import "./App.css";
 import { Header } from "./components/Header";
+import Blogs from "./components/Blogs";
+import Pagination from "./components/Pagination";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { AppContext } from "./context/AppContext";
 
 export default function App() {
-  return <Header />;
+  const { fetchBlogPosts } = useContext(AppContext);
+
+  useEffect(() => {
+    fetchBlogPosts();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Blogs />
+      <Pagination />
+    </div>
+  );
 }
